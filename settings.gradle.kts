c69871b67da818943d0903e38ea4151a186dc048
wn8tdt9sk9-crypto <kryoon82@yu.ac.kr@example.com>
2026-06-01 19:20:54 +0900

    Add yeungnam university

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "product-versions"
-
-pluginManagement {
-    repositories {
-        maven(url = "https://maven-central-eu.storage-download.googleapis.com/maven2")
-        maven(url = "https://cache-redirector.jetbrains.com/plugins.gradle.org")
-        gradlePluginPortal()
-    }
-}
